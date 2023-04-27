ba7707a1683151997a8adf799f0c518c6ad680cd
julius_fro <j.frontzek@campus.tu-berlin.de>
2023-04-27 08:17:45 +0200

    First implementation of ADM1-R3-frac model done

--- ad_meal_prep_control/models.py
+++ ad_meal_prep_control/models.py
@@ -247,13 +247,87 @@ def adm1_r3_frac():
         + c[4] * x[14]
         + c[6] * x[17],
     )
-
+    model.set_rhs(
+        "x_4",
+        c[0] * (xi[3] - x[3]) * u
+        - a[3][0] * theta[0] * x[5]
+        - a[3][1] * theta[1] * x[6]
+        + a[3][2] * theta[2] * x[7]
+        - a[3][3] * theta[3] * x[8]
+        - a[3][4] * theta[5] * x[0] * x[10] / (theta[6] + x[0]) * i_ac,
+    )
+    model.set_rhs(
+        "x_5",
+        c[0] * (xi[4] - x[4]) * u
+        - a[4][0] * theta[0] * x[5]
+        - a[4][1] * theta[1] * x[6]
+        - a[4][2] * theta[2] * x[7]
+        - a[4][3] * theta[3] * x[8]
+        + a[4][4] * theta[5] * x[0] * x[10] / (theta[6] + x[0]) * i_ac,
+    )
+    model.set_rhs(
+        "x_6",
+        c[0] * (theta[8] * xi[5] - x[5]) * u
+        - theta[0] * x[5]
+        + a[5][5] * theta[4] * x[9]
+        + a[5][6] * theta[4] * x[10],
+    )
+    model.set_rhs("x_7", c[0] * ((1.0 - theta[8]) * xi[5] - x[6]) * u - theta[1] * x[6])
+    model.set_rhs(
+        "x_8",
+        c[0] * (xi[7] - x[7]) * u
+        - theta[2] * x[7]
+        + a[7][5] * theta[4] * x[9]
+        + a[7][6] * theta[4] * x[10],
+    )
+    model.set_rhs(
+        "x_9",
+        c[0] * (xi[8] - x[8]) * u
+        - theta[3] * x[8]
+        + a[8][5] * theta[4] * x[9]
+        + a[8][6] * theta[4] * x[10],
+    )
+    model.set_rhs(
+        "x_10",
+        c[0] * (xi[9] - x[9]) * u
+        + a[9][0] * theta[0] * x[5]
+        + a[9][1] * theta[1] * x[6]
+        + a[9][2] * theta[2] * x[7]
+        + a[9][3] * theta[3] * x[8]
+        - theta[4] * x[9],
+    )
+    model.set_rhs(
+        "x_11",
+        c[0] * (xi[10] - x[10]) * u
+        + theta[5] * x[0] * x[10] / (theta[6] + x[0]) * i_ac
+        - theta[4] * x[10],
+    )
     model.set_rhs("x_12", c[0] * (xi[11] - x[11]) * u)
     model.set_rhs("x_13", c[0] * (xi[12] - x[12]) * u)
     model.set_rhs("x_14", c[28] * (x[0] - x[13]) - c[8] * x[13] * s_h_plus)
     model.set_rhs("x_15", c[29] * (x[2] - x[14]) - c[9] * x[14] * s_h_plus)
     model.set_rhs("x_16", c[30] * (x[3] - x[15]) - c[10] * x[15] * s_h_plus)
-
+    model.set_rhs(
+        "x_17",
+        c[21] * x[16] ** 3
+        + c[22] * x[16] ** 2 * x[17]
+        + c[23] * x[16] * x[17] ** 2
+        + c[24] * x[16] ** 2
+        + c[25] * x[16] * x[17]
+        + c[11] * x[1]
+        + c[26] * x[16],
+    )
+    model.set_rhs(
+        "x_18",
+        c[23] * x[17] ** 3
+        + c[22] * x[16] * x[17] ** 2
+        + c[21] * x[16] ** 2 * x[17]
+        + c[25] * x[17] ** 2
+        + c[24] * x[16] * x[17]
+        + c[11] * x[2]
+        - c[11] * x[13]
+        + c[27] * x[17],
+    )
     # Build the model
     model.setup()
 
