ba4347dd635cb6a2f229bdb7d0e736af1f7877b9
JuliusFrontzek <ju-frontzek@gmx.de>
2023-11-28 11:52:35 +0100

    Updated setup file

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(
     name="ad_meal_prep_control",
-    version="0.1",
+    version="0.1.0",
     description="Multi-stage Model Predictive Control algorithm and simulation \
         for a biogas plant (anaerobic digestion) with an optional external gas storage. \
         Considers uncertainties in macronutrients of the fed substrates.",
+    long_description=open("README.md").read(),
     author="Julius Frontzek",
     author_email="ju-frontzek@gmx.de",
     packages=["ad_meal_prep_control"],
@@ -16,4 +17,5 @@ setup(
         "uncertainties",
         "pygame",
     ],
+    data_files=[("./results", [])],
 )
