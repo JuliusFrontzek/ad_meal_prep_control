6b48c1544a7ceaa2affd726980f763624918b1d6
DavidMartinZ <ds.zarate@tum.de>
2025-01-09 00:24:03 +0100

    Scenario 1 ok

--- ad_meal_prep_control/plot_generation/Controller output plotting/Output_Scenario_1b.py
+++ ad_meal_prep_control/plot_generation/Controller output plotting/Output_Scenario_1b.py
@@ -9,13 +9,11 @@ from ad_meal_prep_control.utils import remove_duplicate_labels
 #                  'Scenario_1a_quadratic_no_feedback_mismatch_1std_pr',
 #                  'Scenario_1a_quadratic_no_feedback_mismatch_1std_li']
 
-scenario_names = ["Scenario_1a_quadratic_nominal_feedback_mismatch_5std_3tsap",
-                  'Scenario_1a_quadratic_nominal_ideal_feedback_3tsap',
-                  'Scenario_1a_quadratic_robust_feedback_mismatch_5std_3tsap'
+scenario_names = ["Scenario_1b_quadratic_nominal_feedback_mismatch_5std_3tsap",
                   ]
 
 for scenario in scenario_names:
-    with open(f'../../scenarios/results/plots/{scenario}_substrate_costs.pkl', 'rb') as file:
+    with open(f'../../scenarios/results/plots/{scenario}.pkl', 'rb') as file:
         fig = pkl.load(file)
     mpc = load_results(f'../../scenarios/results/{scenario}_mpc_results.pkl')
     metadata = load_results(f'../../scenarios/results/{scenario}_scenario_meta_data.pkl')
@@ -23,11 +21,11 @@ for scenario in scenario_names:
     if not metadata['feedback']:
         plant_output = np.genfromtxt(f'../../scenarios/results/Plant Output {scenario}.csv', delimiter=' ')
 
-        fig.axes[2].plot(np.linspace(0, 30, num=plant_output.shape[0]), plant_output[:, 1], 'rebeccapurple',
+        fig.axes[3].plot(np.linspace(0, 30, num=plant_output.shape[0]), plant_output[:, 1], 'rebeccapurple',
                          linestyle = 'dotted', label=r'$pH_{controller}$')
-        fig.axes[1].plot(np.linspace(0, 30, num=plant_output.shape[0]), plant_output[:, 0], 'blue',
+        fig.axes[2].plot(np.linspace(0, 30, num=plant_output.shape[0]), plant_output[:, 0], 'blue',
                          linestyle = 'dotted', label=r"$\dot V_{g, controller}$")
-        fig.axes[1].plot(np.linspace(0, 30, num=plant_output.shape[0]), plant_output[:, 2], 'rebeccapurple',
+        fig.axes[2].plot(np.linspace(0, 30, num=plant_output.shape[0]), plant_output[:, 2], 'rebeccapurple',
                          linestyle = 'dotted', label=r"$\dot V_{CH_4, controller}$")
 
         remove_duplicate_labels(fig, 0)
@@ -48,27 +46,30 @@ for scenario in scenario_names:
         predicted_data[:, 0:metadata['t_stp_ahead_pred']+1] = np.NaN
 
         if mpc['mpc'].meta_data['n_robust'] == 0:
-            fig.axes[2].plot(np.linspace(0, 30, num=predicted_data.shape[1]), predicted_data[1, :], 'rebeccapurple',
+            fig.axes[3].plot(np.linspace(0, 30, num=predicted_data.shape[1]), predicted_data[1, :], 'rebeccapurple',
                          linestyle = 'dotted', label=r'$pH_{controller}$')
-            fig.axes[1].plot(np.linspace(0, 30, num=predicted_data.shape[1]), predicted_data[0, :], 'blue',
+            fig.axes[2].plot(np.linspace(0, 30, num=predicted_data.shape[1]), predicted_data[0, :], 'blue',
                          linestyle = 'dotted', label=r"$\dot V_{g, controller}$")
-            fig.axes[1].plot(np.linspace(0, 30, num=predicted_data.shape[1]), predicted_data[2, :], 'rebeccapurple',
+            fig.axes[2].plot(np.linspace(0, 30, num=predicted_data.shape[1]), predicted_data[2, :], 'rebeccapurple',
                          linestyle = 'dotted', label=r"$\dot V_{CH_4, controller}$")
 
         if mpc['mpc'].meta_data['n_robust'] > 0:
-            fig.axes[2].plot(np.linspace(0, 30, num=predicted_data.shape[1]), predicted_data[[2, 3], :].transpose(),
+            fig.axes[3].plot(np.linspace(0, 30, num=predicted_data.shape[1]), predicted_data[[2, 3], :].transpose(),
                              'rebeccapurple',
                              linestyle='dotted', label=r'$pH_{controller}$')
-            fig.axes[1].plot(np.linspace(0, 30, num=predicted_data.shape[1]), predicted_data[[0, 1], :].transpose(),
+            fig.axes[2].plot(np.linspace(0, 30, num=predicted_data.shape[1]), predicted_data[[0, 1], :].transpose(),
                              'blue',
                              linestyle='dotted', label=r"$\dot V_{g, controller}$")
-            fig.axes[1].plot(np.linspace(0, 30, num=predicted_data.shape[1]), predicted_data[[4, 5], :].transpose(),
+            fig.axes[2].plot(np.linspace(0, 30, num=predicted_data.shape[1]), predicted_data[[4, 5], :].transpose(),
                              'rebeccapurple',
                              linestyle='dotted', label=r"$\dot V_{CH_4, controller}$")
 
+
         remove_duplicate_labels(fig, 0)
-        remove_duplicate_labels(fig, 2)
+        remove_duplicate_labels(fig, 1)
         remove_duplicate_labels(fig, 3)
+        remove_duplicate_labels(fig, 4)
+        fig.axes[0].legend(loc ='lower right')
         '''
                 axins_1 = fig.axes[1].inset_axes([0.4, 0.435, 0.25, 0.25],
                     xlim=(5, 10), ylim=(500, 900))
@@ -85,6 +86,5 @@ for scenario in scenario_names:
                                      linestyle='dotted', linewidth=1)
         '''
         #axins_1.grid(True, linestyle='dashed')
-        fig.axes[0].legend(loc='lower right')
-        remove_duplicate_labels(fig, 1, legend_location='center right')
+        remove_duplicate_labels(fig, 2, legend_location='center right')
         plt.savefig(f'../../scenarios/results/plots/predicted_data {scenario}.png')
