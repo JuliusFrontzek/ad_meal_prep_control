e333715d3567f60997557817f42c6db6254b0e99
DavidMartinZ <ds.zarate@tum.de>
2025-01-09 10:48:20 +0100

    Scenario 1 sensitivity ok

---
+++ ad_meal_prep_control/scenarios/scenario_1a_NoFeedback_pr.py
@@ -0,0 +1,58 @@
+from ad_meal_prep_control import simulation
+from ad_meal_prep_control.utils import (
+    ScenarioFactory,
+    CostFunction,
+    ControllerParams,
+    SetpointFunction,
+    Disturbances,
+)
+import numpy as np
+import math
+
+lterm = "10*((model.aux['v_ch4_dot_tank_in'] - model.tvp['v_ch4_dot_tank_in_setpoint'])/model.tvp['v_ch4_dot_tank_in_setpoint'])**2"
+mterm = "100*((model.aux['v_ch4_dot_tank_in'] - model.tvp['v_ch4_dot_tank_in_setpoint'])/model.tvp['v_ch4_dot_tank_in_setpoint'])**2"
+
+cost_func = CostFunction(lterm=lterm, mterm=mterm)
+
+n_days_mpc = 30
+setpoints = np.array([350.0, 550.0, 450.0, 350.0])
+
+ch4_set_point_function = SetpointFunction(
+    setpoints=setpoints,
+    time_points=np.array([3, 6, 9]),
+)
+
+rterms = [
+    f"0.1*(model.u['u_norm'][{i}] - mpc.u_prev['u_norm'][{i}])**2" for i in range(4)
+]
+rterm = " + ".join(rterms)
+
+# No feedback
+controller_params = ControllerParams(
+    mpc_n_horizon=15,
+    mpc_n_robust=0,
+    num_std_devs=1,
+    cost_func=cost_func,
+    substrate_cost_formulation="quadratic",
+    ch4_set_point_function=ch4_set_point_function,
+    rterm=rterm,
+)
+
+kwargs = {
+    "name": "Scenario_1a_quadratic_no_feedback_mismatch_1std_pr",
+    "pygame_vis": False,
+    "mpc_live_vis": False,
+    "n_days_mpc": n_days_mpc,
+    "num_std_devs_sim": 1,
+    "feedback": False,
+    "mismatch": True,
+    "t_stp_ahead_pred": 3
+}
+
+scenario = ScenarioFactory().create_scenario(
+    "methanation", controller_params=controller_params, **kwargs
+)
+
+sim = simulation.Simulation(scenario=scenario)
+sim.setup()
+sim.run()
