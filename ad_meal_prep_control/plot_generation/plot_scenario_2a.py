8c08430cd4062e7eff8b27a2a70c8911fffd5efe
jufro <julius.frontzek@etit.tu-chemnitz.de>
2025-01-28 16:25:03 +0100

    CHP overlay fix attempt 1

--- ad_meal_prep_control/plot_generation/plot_scenario_2a.py
+++ ad_meal_prep_control/plot_generation/plot_scenario_2a.py
@@ -6,12 +6,15 @@ from ad_meal_prep_control.postprocessing import (
 )
 from ad_meal_prep_control import params_R3
 import sys
-from ad_meal_prep_control.plot_generation.Controller_output_plotting.Output_Scenario_2a import controller_plotting_2a
+from ad_meal_prep_control.plot_generation.Controller_output_plotting.Output_Scenario_2a import (
+    controller_plotting_2a,
+)
 
-scenario_names = ['Scenario_2a_dynamic_nominal_feedback_mismatch_3std_8tsap',
-                  'Scenario_2a_dynamic_nominal_ideal_feedback_8tsap',
-                  'Scenario_2a_dynamic_robust_feedback_mismatch_3std_8tsap',
-                 ]
+scenario_names = [
+    "Scenario_2a_dynamic_nominal_feedback_mismatch_3std_8tsap",
+    "Scenario_2a_dynamic_nominal_ideal_feedback_8tsap",
+    "Scenario_2a_dynamic_robust_feedback_mismatch_3std_8tsap",
+]
 plot_save_names = scenario_names
 
 for scenario_name, plot_save_name in zip(scenario_names, plot_save_names):
@@ -24,13 +27,13 @@ for scenario_name, plot_save_name in zip(scenario_names, plot_save_names):
         dpi = int(sys.argv[1])
         show_plot = int(sys.argv[2])
     except IndexError:
-        dpi = 600
-        show_plot = False
+        dpi = 100
+        show_plot = True
 
-    if not 'nominal_feedback' in scenario_name:
+    if not "nominal_feedback" in scenario_name:
         post_processing.plot(
             [
-                #(
+                # (
                 #    "Gas storage  \nlevel" + r" $[\%]$",
                 #    {
                 #        "x_19": PlotVarProperty(
@@ -48,13 +51,13 @@ for scenario_name, plot_save_name in zip(scenario_names, plot_save_names):
                 #            scaling=100.0,
                 #        ),
                 #    },
-                #),
+                # ),
                 (
                     r"$V_{g, tank}$" + "\n" + r"$[m^3]$",
                     {
                         "v_gas_storage": PlotVarProperty(
                             mpl_properties=MPLProperties(
-                                color='black',
+                                color="black",
                                 linewidth=1,
                                 linestyle="-",
                             ),
@@ -67,7 +70,7 @@ for scenario_name, plot_save_name in zip(scenario_names, plot_save_names):
                     {
                         "v_ch4_dot_tank_in": PlotVarProperty(
                             mpl_properties=MPLProperties(
-                                color='blue',
+                                color="blue",
                                 linewidth=1,
                                 linestyle="-",
                             ),
@@ -87,8 +90,9 @@ for scenario_name, plot_save_name in zip(scenario_names, plot_save_names):
                     r"$pH$" + "\n" + r"$[-]$",
                     {
                         "y_4": PlotVarProperty(
-                            mpl_properties=MPLProperties(linewidth=1, linestyle="-",
-                                                     color="black"),
+                            mpl_properties=MPLProperties(
+                                linewidth=1, linestyle="-", color="black"
+                            ),
                             label="$pH_{plant}$",
                         )
                     },
@@ -119,24 +123,31 @@ for scenario_name, plot_save_name in zip(scenario_names, plot_save_names):
             ],
             plot_save_name=plot_save_name,
             constraints=[
-                #Constraint(value=0.0, ax_idx=1),
+                # Constraint(value=0.0, ax_idx=1),
                 Constraint(value=0.0, ax_idx=1),
                 Constraint(value=params_R3.V_GAS_STORAGE_MAX, ax_idx=1),
-                Constraint(value=0.05 * params_R3.V_GAS_STORAGE_MAX, ax_idx=1, color="grey"),
-                Constraint(value=0.95 * params_R3.V_GAS_STORAGE_MAX, ax_idx=1, color="grey"),
+                Constraint(
+                    value=0.05 * params_R3.V_GAS_STORAGE_MAX, ax_idx=1, color="grey"
+                ),
+                Constraint(
+                    value=0.95 * params_R3.V_GAS_STORAGE_MAX, ax_idx=1, color="grey"
+                ),
                 # adapt ylim of plots by adding invisible horizontal lines:
-                Constraint(value=0, ax_idx=2, color="white"),  # gas production lower bound
+                Constraint(
+                    value=0, ax_idx=2, color="white"
+                ),  # gas production lower bound
                 Constraint(value=7.6, ax_idx=3, color="white"),  # pH upper bound
                 Constraint(value=0, ax_idx=4, color="white"),  # inhibtion lower bound
             ],
             dpi=dpi,
             show_plot=show_plot,
             height_ratios=[2, 2, 2, 1, 1],
+            color_background_indices=(1,),
         )
     else:
         post_processing.plot(
             [
-                #(
+                # (
                 #    "Gas storage  \nlevel" + r" $[\%]$",
                 #    {
                 #        "x_19": PlotVarProperty(
@@ -154,13 +165,13 @@ for scenario_name, plot_save_name in zip(scenario_names, plot_save_names):
                 #            scaling=100.0,
                 #        ),
                 #    },
-                #),
+                # ),
                 (
                     r"$V_{g, tank}$" + "\n" + r"$[m^3]$",
                     {
                         "v_gas_storage": PlotVarProperty(
                             mpl_properties=MPLProperties(
-                                color='black',
+                                color="black",
                                 linewidth=1,
                                 linestyle="-",
                             ),
@@ -173,7 +184,7 @@ for scenario_name, plot_save_name in zip(scenario_names, plot_save_names):
                     {
                         "v_ch4_dot_tank_in": PlotVarProperty(
                             mpl_properties=MPLProperties(
-                                color='blue',
+                                color="blue",
                                 linewidth=1,
                                 linestyle="-",
                             ),
@@ -193,8 +204,9 @@ for scenario_name, plot_save_name in zip(scenario_names, plot_save_names):
                     r"$pH$" + "\n" + r"$[-]$",
                     {
                         "y_4": PlotVarProperty(
-                            mpl_properties=MPLProperties(linewidth=1, linestyle="-",
-                                                     color="black"),
+                            mpl_properties=MPLProperties(
+                                linewidth=1, linestyle="-", color="black"
+                            ),
                             label="$pH_{plant}$",
                         )
                     },
@@ -225,31 +237,35 @@ for scenario_name, plot_save_name in zip(scenario_names, plot_save_names):
             ],
             plot_save_name=plot_save_name,
             constraints=[
-                #Constraint(value=0.0, ax_idx=1),
+                # Constraint(value=0.0, ax_idx=1),
                 Constraint(value=0.0, ax_idx=1),
                 Constraint(value=params_R3.V_GAS_STORAGE_MAX, ax_idx=1),
-                Constraint(value=0.05 * params_R3.V_GAS_STORAGE_MAX, ax_idx=1, color="grey"),
-                Constraint(value=0.95 * params_R3.V_GAS_STORAGE_MAX, ax_idx=1, color="grey"),
+                Constraint(
+                    value=0.05 * params_R3.V_GAS_STORAGE_MAX, ax_idx=1, color="grey"
+                ),
+                Constraint(
+                    value=0.95 * params_R3.V_GAS_STORAGE_MAX, ax_idx=1, color="grey"
+                ),
             ],
             other_inset_axes=[
-                #{
+                # {
                 #    "plot_idx": 1,
                 #    "days": (18, 23),
                 #    "ylimit": (-10, 120),
                 #    "inset_axis_specs": (0.4, 0.2, 0.35, 0.35)
-                #},
-                #{
+                # },
+                # {
                 #    "plot_idx": 1,
                 #    "days": (18, 23),
                 #    "ylimit": (20, 300),
                 #    "inset_axis_specs": (0.4, 0.2, 0.35, 0.35)
-                #},
-                #{
+                # },
+                # {
                 #    "plot_idx": 3,
                 #    "days": (3, 5),
                 #    "ylimit": (100, 700),
                 #    "inset_axis_specs": (0.4, 0.4, 0.25, 0.25)
-                #},
+                # },
                 # {
                 #    "plot_idx": 3,
                 #    "days": (17.1, 17.4),
@@ -260,5 +276,6 @@ for scenario_name, plot_save_name in zip(scenario_names, plot_save_names):
             dpi=dpi,
             show_plot=show_plot,
             height_ratios=[2, 2, 2, 1, 1],
+            color_background_indices=(1,),
         )
 controller_plotting_2a(scenario_names)
