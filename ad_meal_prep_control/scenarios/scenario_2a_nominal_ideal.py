e792f5045006c209d61a647fae493594f541400c
Simon Hellmann <>
2025-01-30 10:14:23 +0100

    update scenario 2a (3 SDs) and plots

--- ad_meal_prep_control/scenarios/scenario_2a_nominal_ideal.py
+++ ad_meal_prep_control/scenarios/scenario_2a_nominal_ideal.py
@@ -9,8 +9,9 @@ from ad_meal_prep_control.utils import (
 from ad_meal_prep_control.params_R3 import P_el_chp
 import numpy as np
 
-lterm = ("(0.5*(model.x['x_19'] + model.x['x_20'] + model.aux['V_H2O']/V_GAS_STORAGE_MAX - 0.4)**2 +"
-         "+ 50*(model.x['x_19'] + model.x['x_20'] + model.aux['V_H2O']/V_GAS_STORAGE_MAX - 0.4)**4)"
+fill_level_setpoint = 0.4
+lterm = (f"(0.5*(model.x['x_19'] + model.x['x_20'] + model.aux['V_H2O']/V_GAS_STORAGE_MAX - {fill_level_setpoint})**2 +"
+         f"+ 50*(model.x['x_19'] + model.x['x_20'] + model.aux['V_H2O']/V_GAS_STORAGE_MAX - {fill_level_setpoint})**4)"
          )
 
 mterm = "model.tvp['dummy_tvp']"
