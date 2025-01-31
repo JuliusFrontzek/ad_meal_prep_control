59a86282677da176808954ad2f165bbdd090308d
Simon Hellmann <>
2025-01-31 11:01:12 +0100

    update boxplots

--- ad_meal_prep_control/plot_generation/horizontal_boxplot_macronutrients.py
+++ ad_meal_prep_control/plot_generation/horizontal_boxplot_macronutrients.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+# import matplotlib.colors as mcolors
 import pandas as pd
+from pathlib import Path
 from webencodings import labels
 
 ##### get the fuckin data from excel, motherfucker!
@@ -17,9 +19,7 @@ dfs_substrates = [df_cornsilage, df_grassilage, df_sugarbeet, df_manure]
 
 sub_names = ['corn silage', 'grass silage', 'sugar beet silage', 'cattle manure']
 sub_colors = ['orange', 'limegreen', 'deeppink', 'saddlebrown']
-
-cornsilage_carbs = df_cornsilage['Xch']
-sugarbeet_carbs = df_sugarbeet['Xch']
+sub_colors_dark = ['darkorange', 'green', 'mediumvioletred', 'maroon']
 
 df_carbs = pd.concat(
     [df_cornsilage.iloc[:, 0], df_grassilage.iloc[:, 0], df_sugarbeet.iloc[:, 0], df_manure.iloc[:, 0]], axis=1)
@@ -32,50 +32,52 @@ for k in range(3):
     dfs[k].columns = sub_names
 
 # Create the figure and axes
-fig, axes = plt.subplots(nrows=3, figsize=(5, 4))
+fig, axes = plt.subplots(nrows=3, figsize=(4, 5))
+right_axes = []  # Store the right y-axes for later iteration
 
 # iterate over macro nutrients:
 for k, ax in enumerate(axes):
-    # iterate over substrates: 
+    axR = ax.twinx()  # instantiate a second Axes that shares the same x-axis
+    # iterate over substrates:
     for subk in range(len(dfs_substrates)):
-        ax.boxplot(dfs_substrates[subk].iloc[:, k],
-                   vert=False,  # horizontal boxplots
-                   patch_artist=True,
-                   boxprops=dict(facecolor=sub_colors[subk], color=sub_colors[subk], alpha=0.5),
-                   # Box fill color with transparency
-                   whiskerprops=dict(color=sub_colors[subk]),  # Whisker color
-                   capprops=dict(color=sub_colors[subk]),  # Cap color
-                   medianprops=dict(color=sub_colors[subk]),  # Median line color
-                   showfliers=False,  # ignore outliers
-                   positions=[subk]
-                   # flierprops=dict(marker='o', color='pink', alpha=0.5)  # outlier color
-                   )
+        axR.boxplot(dfs_substrates[subk].iloc[:, k],
+                    vert=False,  # horizontal boxplots
+                    patch_artist=True,
+                    boxprops=dict(facecolor=sub_colors[subk], color=sub_colors_dark[subk], alpha=0.4),
+                    # Box fill color with transparency
+                    whiskerprops=dict(color=sub_colors_dark[subk]),  # Whisker color
+                    capprops=dict(color=sub_colors[subk]),  # Cap color
+                    medianprops=dict(color=sub_colors_dark[subk]),  # Median line color
+                    positions=[subk],
+                    showfliers=False,  # ignore outliers
+                    # flierprops=dict(marker='o', color='pink', alpha=0.5)  # outlier color
+                    )
+    right_axes.append(axR)  # Store the right y-axis (of complete subplot) for later use
 
 subtitles = ['carbohydrates', 'proteins', 'lipids']
 xlabels = [r"$\xi_{ch}$", r"$\xi_{pr}$", r"$\xi_{li}$"]
-ylabels = ['mais', 'ruebe']
-for k, ax in enumerate(axes):
+
+# Iterate through all subplots and adjust labels
+for k, ax, axR in zip(range(3), axes, right_axes):
     ax.set_title(subtitles[k])
     ax.set_xlabel(xlabels[k])
-    ax.set_yticks(list(range(4)), sub_names)  # Set y-ticks for groups
-    ax.grid(True, linestyle="--")
+    # Mute the left y-axis
+    ax.set_ylabel("")  # Remove the left y-axis label
+    ax.tick_params(left=False, labelleft=False)  # Disable left ticks and labels
+    # Add labels to the right y-axis:
+    axR.set_yticks(list(range(4)), sub_names)  # Set y-ticks for groups
 
 # Show the plot
 plt.tight_layout()
+fig.savefig(
+    fname=str(
+        Path(
+            "../../results",
+            "plots",
+            f"xi_distributions.png",
+        )
+    ),
+    dpi=600,
+    format="png",
+)
 plt.show()
-
-bla = 1
-# Customize boxplots
-# for idx, patch in enumerate(box['boxes']):
-#    #patch.set_facecolor('orange')  # Set box color: specific of each substrate
-#    patch.set_edgecolor('orange')
-#    patch.set_wiskers('orange')
-#    patch.set_alpha(0.3)  # Set transparency (alpha)
-
-# Add labels for each boxplot
-# ax.set_yticklabels(['substrate'])  # ggf weglassen XY
-
-# # Add title and labels
-# ax.set_title(['carbohydrates', 'proteins', 'lipids'])
-# ax.set_xlabel(r"$\xi_{ch} [\mathrm{g} \mathrm{L}^{-1}]$")
-# ax.set_ylabel('corn silage')
