981546f59455c23d7ef8b4158ac3c4f743906f6f
Simon Hellmann <>
2025-07-24 12:17:22 +0200

    update Sc. 2a for only 1 SD (now show in SI)

--- tests/test_total_computing_time.py
+++ tests/test_total_computing_time.py
@@ -17,15 +17,15 @@ def read_computation_times(file_path):
 
     return f"{int(hours):02}:{int(minutes):02}:{int(seconds):02}", round(total_seconds)
 
-my_scenario = '2a_nominal_1sigma'  # '1b' '2a_robust', '2a_nominal', '2c', '2c_uninhibited', '2a_nominal_1sigma', '2a_robust_1sigma'
+my_scenario = '2a_robust_1sigma'  # '1b' '2a_robust', '2a_nominal', '2c', '2c_uninhibited', '2a_nominal_1sigma', '2a_robust_1sigma'
 if my_scenario == '1b':
     scen_name = 'Scenario_1b_quadratic_robust_feedback_mismatch_1.5std_3tsap'
 elif my_scenario == '2a_nominal':
-    scen_name = 'Scenario_2a_dynamic_nominal_feedback_mismatch_2std_8tsap'
+    scen_name = 'Scenario_2a_dynamic_nominal_feedback_mismatch_2std_12tsap'
 elif my_scenario == '2a_nominal_1sigma':
     scen_name = 'Scenario_2a_dynamic_nominal_feedback_mismatch_1std_12tsap'
 elif my_scenario == '2a_robust':
-    scen_name = 'Scenario_2a_dynamic_robust_feedback_mismatch_2std_8tsap'
+    scen_name = 'Scenario_2a_dynamic_robust_feedback_mismatch_2std_12tsap'
 elif my_scenario == '2a_robust_1sigma':
     scen_name = 'Scenario_2a_dynamic_robust_feedback_mismatch_1std_12tsap'
 elif my_scenario == '2c':
@@ -36,4 +36,4 @@ elif my_scenario == '2c_uninhibited':
 my_path = '../ad_meal_prep_control/scenarios/results/'
 file_name = scen_name + '_mpc_computation_times_mikro_secs.txt'
 run_time_hh_mm_ss, run_time_sec = read_computation_times(my_path + file_name)
-print(f"\ncomputation time was: {run_time_hh_mm_ss} or {run_time_sec} seconds.")
+print(f"\ncomputation time was: {run_time_hh_mm_ss}, i.e., {run_time_sec} seconds.")
